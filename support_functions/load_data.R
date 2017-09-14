756789797d94306d9aa0dd2b7109783e6023c55c
Albert Xue <xuebert@u.northwestern.edu>
2017-09-14 15:39:58 -0500

    New way to load data

--- support_functions/load_data.R
+++ support_functions/load_data.R
@@ -1,9 +1,15 @@
 load_data <- function(filename) {
   
   # read the file
-  raw_data = read.csv(filename)
-  
+  nc = max(count.fields(filename, sep="\t"))
+  raw_data = read.table(filename, sep="\t", header=FALSE, col.names = 1:nc, fill=TRUE, colClasses="numeric")
+
   raw_data = as.matrix(raw_data)
+  
+  # if last column is all na, remove it
+  if (sum(is.na(raw_data[,ncol(raw_data)])) == ncol(raw_data)) {
+    raw_data = raw_data[, -ncol(raw_data), drop = F]
+  }
   
   return(raw_data)
   
