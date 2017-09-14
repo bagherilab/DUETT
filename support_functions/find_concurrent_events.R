2a2723bd473e986fa8659875a9cb54c4afe36d7f
Albert Xue <xuebert@u.northwestern.edu>
2017-09-14 15:39:10 -0500

    Added support for concurrent events

--- support_functions/find_concurrent_events.R
+++ support_functions/find_concurrent_events.R
@@ -1,24 +1,21 @@
-find_concurrent_events <- function(max_concurrent_distance = 2, comparison_point = "start", event_types = c(-3,-1,1,3), merge_3 = 2) {
+find_concurrent_events <- function(event_locations, concurrent_distance = 2, comparison_point = "start", event_types = c(-3,-2,-1,1,2,3), merge_3 = 2) {
   
-  concurrent_events = rep(list(matrix(NA, nrow = 0, ncol = 4, dimnames = list(NULL, c("row1", "col1", "row2", "col2")))), 2)
-  event_points = rep(list(matrix(NA, ncol = 4, nrow = 0, dimnames = list(NULL, c("col", "start", "middle", "end")))), 2)
+  concurrent_events = matrix(NA, nrow = 0, ncol = 4, dimnames = list(NULL, c("row1", "col1", "row2", "col2")))
+  event_points = matrix(NA, ncol = 4, nrow = 0, dimnames = list(NULL, c("col", "start", "middle", "end")))
   
-  for (n_direction in 1:2) {
-    
-    # merge 3 into 2 or 1
-    event_locations[[n_direction]][event_locations[[n_direction]] == 3] = merge_3
-    event_locations[[n_direction]][event_locations[[n_direction]] == -3] = -merge_3
-    
-    event_points[[n_direction]] = find_runs(event_locations[[n_direction]], event_types = event_types)
-    
-    for (n_row1 in 1:(nrow(event_points[[n_direction]])-1)) {
-      for (n_row2 in (n_row1+1):nrow(event_points[[n_direction]])) {
-        distance = abs(event_points[[n_direction]][n_row1, comparison_point] - event_points[[n_direction]][n_row2, comparison_point])
-        col_diff = abs(event_points[[n_direction]][n_row1, "col"] - event_points[[n_direction]][n_row2, "col"]) # record difference in col (to make sure not concurrent event is not in same column)
-        
-        if ((distance <= max_concurrent_distance) & (col_diff > 0)) {
-          concurrent_events[[n_direction]] = rbind(concurrent_events[[n_direction]], c(event_points[[n_direction]][n_row1, c(comparison_point, "col")], event_points[[n_direction]][n_row2, c(comparison_point, "col")]))
-        }
+  # merge 3 into 2 or 1
+  event_locations[event_locations == 3] = merge_3
+  event_locations[event_locations == -3] = -merge_3
+  
+  event_points = find_runs(event_locations, event_types = event_types)
+  
+  for (n_row1 in 1:(nrow(event_points)-1)) {
+    for (n_row2 in (n_row1+1):nrow(event_points)) {
+      distance = abs(event_points[n_row1, comparison_point] - event_points[n_row2, comparison_point])
+      col_diff = abs(event_points[n_row1, "col"] - event_points[n_row2, "col"]) # record difference in col (to make sure not concurrent event is not in same column)
+      
+      if ((distance <= concurrent_distance) & (col_diff > 0)) {
+        concurrent_events = rbind(concurrent_events, c(event_points[n_row1, c(comparison_point, "col")], event_points[n_row2, c(comparison_point, "col")]))
       }
     }
   }
--- support_functions/plotting/make_visual.R
+++ support_functions/plotting/make_visual.R
@@ -1,9 +1,10 @@
-make_visual <- function(data_mat, event_locations, color_scale = NULL, numbering = T, numbering_interval = 5, lwd = 0.01, fg = "black", circle_size_override = NULL) {
+make_visual <- function(data_mat, event_locations, concurrent_events, color_scale = NULL, numbering = T, numbering_interval = 5, lwd = 0.01, fg = "black", circle_size_override = NULL) {
   
   source("support_functions/utility_functions.R")
   source("support_functions/color_to_hex.R")
   source("support_functions/plotting/make_boxes.R")
   source("support_functions/plotting/axis_labels.R")
+  source("support_functions/plotting/make_visual_concurrent.R")
   
   # set colors
   # event_colors = list(upswing = color_to_hex("red", 0.1), downswing = color_to_hex("blue", 0.1)) # transparent
@@ -12,11 +13,17 @@ make_visual <- function(data_mat, event_locations, color_scale = NULL, numbering
   library(RColorBrewer)
   grey_color = colorRampPalette(brewer.pal(n = 7, name ="Greys"))(100)
   
+  # make grey boxes for the data
   make_boxes(data_mat = data_mat, color_scale = grey_color, lwd = lwd, fg = clear_color)
   if (numbering) {axis_labels(n_row = nrow(data_mat), n_col = ncol(data_mat), numbering_interval = numbering_interval)}
   
+  # add swing and ramp events
   par(new = T)
   make_visual_PID(event_locations, event_draw = c(-1,1, -3,3), ramp_draw = c(-1,1), event_colors = event_colors)
-  par(new = F)
+  par(new = T)
   
+  # add concurent events
+  num_rows = nrow(event_locations)
+  make_visual_concurrent(concurrent_events, num_rows)
+  par(new = F)
 }
