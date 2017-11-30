e13fd5ecf25147c095e0daa4f039ad3bf6da848e
Xuebert@gmail.com <xuebert@u.northwestern.edu>
2017-11-29 18:24:33 -0600

    Updating axis labels to have offset.  still need to add to front gui

--- support_functions/plotting/axis_labels.R
+++ support_functions/plotting/axis_labels.R
@@ -7,11 +7,15 @@ axis_labels <- function(n_row, n_col, numbering_interval = 5, axis_label_resize 
     return(seq_var)
   }
   
+  # hack to make side labels offset by 20
+  side_offset = 20
+  
   if (axis_label_resize != 0) {
     # left labels
-    text(rep(1, floor(n_row/numbering_interval)), c(n_row, seq(n_row - (numbering_interval-1), 1, -numbering_interval)), labels = 20+make_sequence(1, n_row), cex = 1.5 * axis_label_resize, pos = 2)
+    text(rep(1, floor(n_row/numbering_interval)), c(n_row, seq(n_row - (numbering_interval-1), 1, -numbering_interval)), labels = make_sequence(1 + side_offset, n_row + side_offset), cex = 1.5 * axis_label_resize, pos = 2)
     # right labels
-    text(rep(n_col, floor(n_row/numbering_interval)), c(n_row, seq(n_row - (numbering_interval-1), 1, -numbering_interval)), labels = 20+make_sequence(1, n_row), cex = 1.5 * axis_label_resize, pos = 4)
+    text(rep(n_col, floor(n_row/numbering_interval)), c(n_row, seq(n_row - (numbering_interval-1), 1, -numbering_interval)), labels = make_sequence(1 + side_offset, n_row + side_offset), cex = 1.5 * axis_label_resize, pos = 4)
+    
     # top labels
     text(c(1, seq(numbering_interval, n_col, by = numbering_interval)), rep(n_row, floor(n_col / numbering_interval)), make_sequence(1, n_col), cex = 1.5 * axis_label_resize, pos = 3)
     # bottom labels
