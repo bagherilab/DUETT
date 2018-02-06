be5e94b45fd46bdefc3ba7d73474a4e4b7dc0660
Xuebert@gmail.com <xuebert@u.northwestern.edu>
2018-02-06 15:31:18 -0600

    More replicate analysis. Start working on individual riboswitch replicates and compare to subtraction

--- analysis/SRP/replicates/replicate_similarity.R
+++ analysis/SRP/replicates/replicate_similarity.R
@@ -1,6 +1,7 @@
 rm(list=ls())
 
-setwd("~/Dropbox/Albert Xue/Research/Deployment/SHAPE-Seq_event_detector/")
+# setwd("~/Dropbox/Albert Xue/Research/Deployment/SHAPE-Seq_event_detector/")
+setwd("C:/Users/Xuebert/Dropbox/Albert Xue/Research/Deployment/SHAPE-Seq_event_detector/")
 
 source("support_functions/ShapeSeq_events.R")
 source("support_functions/find_concurrent_events.R")
@@ -31,9 +32,10 @@ event_locations_shared_all[which(shared_all, arr.ind = T)] = event_locations1[wh
 event_locations_shared_2 = event_locations1 * 0
 event_locations_shared_2[which(shared_2, arr.ind = T)] = event_locations1[which(shared_2, arr.ind = T)]
 
-concurrent_events = find_concurrent_events(event_locations_shared_all, 2, event_types = c(-1,1))
+concurrent_events_all = find_concurrent_events(event_locations_shared_all, 2, event_types = c(-1,1))
+concurrent_events_2 = find_concurrent_events(event_locations_shared_2, 2, event_types = c(-1,1))
 
 pdf("analysis/SRP/replicates/replicate_shared_events.pdf", width = 14, height = 14)
-make_visual(data_mat, event_locations_shared_all, concurrent_events)
-make_visual(data_mat, event_locations_shared_2, concurrent_events)
+make_visual(data_mat, event_locations_shared_all, concurrent_events_all)
+make_visual(data_mat, event_locations_shared_2, concurrent_events_2)
 dev.off()
